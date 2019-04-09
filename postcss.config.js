18942f8db4ea046bdd3d2b805700d6082f99b741
楊育瑄 <yuhsuanya@gmail.com>
2019-04-09 21:36:04 +0800

    final

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,15 @@
-const 	_nested = require('postcss-nested'),
-	_cssvars = require('postcss-simple-vars'),
-	_autoprefixer = require('autoprefixer'),
-	_import = require('postcss-import'),
-	_mixins = require('postcss-mixins');
+const _nested = require('postcss-nested'),
+_cssvars = require('postcss-simple-vars'),
+_autoprefixer = require('autoprefixer'),
+_import = require('postcss-import'),
+_mixins = require('postcss-mixins');
 
-      module.exports = {
-	plugins: [
-	      import(),
-	      _nested(),
-	      _cssvars(),
-	      _autoprefixer(),
-	      _import(),
-	      _mixins()
-	]
-      }
+module.exports = {
+plugins: [
+_import(),
+_cssvars(),
+_nested(),
+_mixins(),
+_autoprefixer()
+]  
+};
